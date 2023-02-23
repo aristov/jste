047278034e8070ff7068930c1ca5082d03f12015
Vyacheslav Aristov <vv.aristov@gmail.com>
2023-02-23 09:14:57 +0800

    attrHandlers.className handler implementation

--- test/attrHandlers.test.js
+++ test/attrHandlers.test.js
@@ -20,3 +20,19 @@ test('style: object', t => {
 
   t.is(result.toString(), '<div style="display:grid;grid-template-columns:auto 1fr auto;grid-gap:1em;"></div>')
 })
+
+test('className: string', t => {
+  const result = jste('div', {
+    className : 'foo bar baz',
+  })
+
+  t.is(result.toString(), '<div class="foo bar baz"></div>')
+})
+
+test('className: array', t => {
+  const result = jste('div', {
+    className : [false, 'foo', null, 'bar', undefined, 'baz', '', 'bat', 0, 'xyz', NaN],
+  })
+
+  t.is(result.toString(), '<div class="foo bar baz bat xyz"></div>')
+})
