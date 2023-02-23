61da30778dd937ae8d7a21130aad8f3ff37b2c7e
Vyacheslav Aristov <vv.aristov@gmail.com>
2023-02-23 16:01:14 +0800

    Refactoring

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @module jste
  * @author Vyacheslav Aristov <vv.aristov@gmail.com>
  */
-module.exports = require('./lib/elements')
+module.exports = require('./lib/jste')
 
 if(typeof process !== 'undefined') {
   module.exports.__express = eval('require("./engine")')
