41f43a4638950ec6616912ba14c4c71575d5ead0
Vyacheslav Aristov <vv.aristov@gmail.com>
2023-02-23 09:03:32 +0800

    attrHandlers.style handler implementation

---
+++ test/attrHandlers.test.js
@@ -0,0 +1,22 @@
+const test = require('ava')
+const jste = require('..')
+
+test('style: string', t => {
+  const result = jste('div', {
+    style : 'display:flex;font-size:2em;',
+  })
+
+  t.is(result.toString(), '<div style="display:flex;font-size:2em;"></div>')
+})
+
+test('style: object', t => {
+  const result = jste('div', {
+    style : {
+      display : 'grid',
+      gridTemplateColumns : 'auto 1fr auto',
+      gridGap : '1em',
+    },
+  })
+
+  t.is(result.toString(), '<div style="display:grid;grid-template-columns:auto 1fr auto;grid-gap:1em;"></div>')
+})
